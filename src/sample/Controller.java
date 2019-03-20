f17770f9aeabb2148c65d06b30d2b873b045a041
SuperRsDev <superemail@etf.unsa.ba>
2019-03-20 10:39:28 +0100

    Radio na izmjeni najduze rijeci sa velikim slovima, ima problem kod splitanja na whitespace

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,53 +1,30 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Controller {
-    public TextField uneseniBrojN;
-    public GridPane brojeviLista;
+    public TextField uneseniTekst;
+    public Label izmjenjeniTekst;
 
-    public void UnosBroja(ActionEvent actionEvent) {
-        var uneseniBroj = uneseniBrojN.getText();
-        var broj = Integer.parseInt(uneseniBroj);
-        var brojeviDjeljivi = BrojeviDjeljiviSaSumomSvojihCifara(broj);
-        ShowListuBrojevaNaPanel(brojeviDjeljivi);
+    public void UnosTeksta(ActionEvent actionEvent) {
+        var tekst = uneseniTekst.getText();
+        var noviTekstSaNajduzomRijeci = NajduzaRijecUVelikaSlova(tekst);
+        izmjenjeniTekst.setText(noviTekstSaNajduzomRijeci);
     }
 
-    private void ShowListuBrojevaNaPanel(List<Integer> brojevi) {
-        int row = 0;
-        for (var broj : brojevi) {
-            var label = new Label(broj.toString());
-            brojeviLista.add(label, 0, row);
-            row++;
-        }
-    }
+     private String NajduzaRijecUVelikaSlova(String tekst) {
+        String[] rijeci = tekst.split("\\\\s+");
+        Arrays.sort(rijeci, Comparator.comparingInt(String::length).reversed()); // Sortiranje u desc orderuv
+        var najduzaRijec = rijeci[0];
+        var najduzaRijecVelikaSlova = najduzaRijec.toUpperCase();
+        System.out.println(najduzaRijecVelikaSlova + "  "  +najduzaRijec);
+        return tekst.replaceAll("\\b"+ najduzaRijec +"\\b", najduzaRijecVelikaSlova);
+     }
 
-    private List<Integer> BrojeviDjeljiviSaSumomSvojihCifara(int n) {
-        List<Integer> brojeviDjeljiviSaSumomCifara = new ArrayList<>();
-        for(int i = 1;i < n;i++) {
-            int sumaCifara = SumaCifara(i);
-            if(i % sumaCifara == 0) {
-                brojeviDjeljiviSaSumomCifara.add(i);
-            }
-        }
-
-        return brojeviDjeljiviSaSumomCifara;
-    }
-
-    private int SumaCifara(int n) {
-        int suma = 0;
-        while (n != 0) {
-            suma += n % 10;
-            n = n / 10;
-        }
-        return suma;
-    }
 }
+
